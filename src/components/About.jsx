4c84fbc784b659600518499e0b25fea6441b5826
Andrew Bethel <andrew@striking.ly>
2015-12-13 17:48:28 -0600

    social media icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,8 +16,28 @@ export const About = React.createClass({
           <div className="s-about-title">About Me</div>
         </div>
         <div className="container">
-          <div className="col-xs-4"></div>
-          <div className="col-xs-8"></div>
+          <div className="col-md-8 col-md-offset-2 s-about-body">
+            I'm a talented programmer with an increasingly diverse skillset. I've worked with applications built on a number of different stacks, and I am familiar with many of the modern libraries used in web development.  Feel free to find me through any of the social media links below, and don't forget to peruse my github repositories!
+          </div>
+          <div className="col-md-8 col-md-offset-2 s-about-body">
+            <ul className="social-media">
+              <li>
+                <a href="http://github.com/abeth86">
+                  <i className="fa fa-github-alt"></i>
+                </a>
+              </li>
+              <li>
+                <a href="http://www.google.com/+AndrewBethel1986">
+                  <i className="fa fa-google-plus"></i>
+                </a>
+              </li>
+              <li>
+                <a href="http://www.linkedin.com/in/abethel86">
+                  <i className="fa fa-linkedin fa-fw"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     )
