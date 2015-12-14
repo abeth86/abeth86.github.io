c3950e997d44e9100f2181f2dd7cd83b24d7bbea
Andrew Bethel <andrew@striking.ly>
2015-12-13 20:25:28 -0600

    add images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,6 +12,15 @@ export const Home = React.createClass({
       <div className="s-home">
         <div className="container">
           <div className="col-xs-6 col-md-6 col-xs-offset-3 col-md-offset-3 s-home-title">abethel.io</div>
+          <div className="col-xs-6 col-md-6 col-xs-offset-3 col-md-offset-3 s-home-icons">
+            <img className="skill-icon" src="/img/html5-min.png" />
+            <img className="skill-icon" src="/img/css3-min.png" />
+            <img className="skill-icon" src="/img/less-min.png" />
+            <img className="skill-icon" src="/img/es6-min.png" />
+            <img className="skill-icon" src="/img/reactjs-min.png" />
+            <img className="skill-icon" src="/img/angularjs-min.png" />
+            <img className="skill-icon" src="/img/nodejs-min.png" />
+          </div>
         </div>
       </div>
     )
