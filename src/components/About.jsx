57b8cb40965bbc6de66d3a575ca9e35101fc0811
awitherspoon <a-witherspoon@outlook.com>
2017-02-18 18:09:29 -0600

    new bundle

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -112,7 +112,7 @@ export class About extends React.Component {
         <div className='divider'>
           <Col xs={12} md={12} className='resume'>
             <div className='res-button'>
-              <a href="/dist/andrew_resume.pdf" className='btn btn-default btn-resume'>
+              <a href="/andrew_resume.pdf" className='btn btn-default btn-resume'>
                 <i className='fa fa-cloud-download icon-before'></i>
                 Get CV
               </a>
